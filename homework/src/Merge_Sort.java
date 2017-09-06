3475e565837d6a56dff2bf20cc5698fe409feaf0
garen <garend95@gmail.com>
2017-09-06 21:02:40 +0400

    Fixed several indexing issues, added comments

--- homework/src/Merge_Sort.java
+++ homework/src/Merge_Sort.java
@@ -7,50 +7,65 @@ public class Merge_Sort {
 
         int i = 0;
         while (i < array.length) {
-            System.out.println(array[i]);
+            System.out.print(array[i] + " ");   //prints the sorted elements
+            i++;
         }
 
     }
 
-    private void MergeSort(int[] array, int startIndex, int endIndex){
-        if (endIndex > startIndex){
-            int middle = startIndex + (endIndex - startIndex)/2 ;   //sometimes the start index isn't 0
-            MergeSort(array, startIndex, middle);
-            MergeSort(array, middle + 1, endIndex);
-            Merge(array, startIndex, middle, endIndex);
+        private void MergeSort(int[] array, int startIndex, int endIndex){
+            if (endIndex > startIndex){
+                int middle = startIndex + (endIndex - startIndex)/2 ;   //sometimes the start index isn't 0
+                MergeSort(array, startIndex, middle);
+                MergeSort(array, middle + 1, endIndex);
+                Merge(array, startIndex, middle, endIndex); // this function will merge from the bottom (where only single elements lie) to the top
         }
     }
 
     private void Merge(int[] array, int startIndex, int middle, int endIndex){
-        int leftArray[] = new int[middle - startIndex];    //these arrays are temporary so that we would not
-        int rightArray[] = new int[endIndex - middle + 1]; //have to replace the original positions
+        int leftArray[] = new int[middle - startIndex + 1];    //these arrays are temporary so that we would not
+        int rightArray[] = new int[endIndex - middle]; //have to replace the original positions
 
-        int i = 0;
-        while (i < middle - startIndex) {
-            leftArray[i] = array[i];
-            i++;
+        int i = startIndex, j = 0, k = 0;
+        while (i <= middle) {
+            leftArray[j] = array[i];
+            i++; j++;
         }
-        while (i < array.length) {
-            rightArray[i] = array[i];
-            i++;
+
+        while (i <= endIndex) {
+            rightArray[k] = array[i];
+            i++; k++;
         }
+
 
         i = startIndex;  //index of the original array as we will begin to rewrite it
-        int j = 0, k = 0; //indexes of the left and right arrays respectively
+        j = 0; //index of the left array
+        k = 0; //index of the right array
 
-        while (j < leftArray.length || k < rightArray.length) {
-            if (leftArray[j] < rightArray[k])
-            {
+
+        while (j < leftArray.length && k < rightArray.length) {
+            if (leftArray[j] < rightArray[k]) {
                 array[i] = leftArray[j];
-                i++; j++;
-            }
-            else if (leftArray[j] >= rightArray[k])
-            {
+                i++;
+                j++;
+
+            } else {
                 array[i] = rightArray[k];
-                i++; k++;
+                i++;
+                k++;
             }
-            else if (j == leftArray.length - 1) array[i] = rightArray[k]; // if the left array has been fully used
-            else if (k == rightArray.length - 1) array[i] = leftArray[j]; // if the right array has been fully used
+        }
+
+        while (j < leftArray.length) {  //in case one of the arrays finishes faster than the other
+            array[i] = leftArray[j];
+            i++;
+            j++;
+        }
+
+        while (i < rightArray.length) {
+            array[i] = leftArray[k];
+            i++;
+            k++;
         }
     }
 }
