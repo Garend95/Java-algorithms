a0f15e913bebe37122ac4688ad81a9e67f8e82c2
garen <garend95@gmail.com>
2017-09-06 19:02:07 +0400

    left array and right array created for Merge function definition

--- homework/src/Merge_Sort.java
+++ homework/src/Merge_Sort.java
@@ -8,7 +8,7 @@ public class Merge_Sort {
 
     public void MergeSort(int[] array, int startIndex, int endIndex){
         if (endIndex > startIndex){
-            int middle = startIndex + (endIndex - startIndex)/2 ;
+            int middle = startIndex + (endIndex - startIndex)/2 ;   //sometimes the start index isn't 0
             MergeSort(array, startIndex, middle);
             MergeSort(array, middle + 1, endIndex);
 
@@ -16,6 +16,21 @@ public class Merge_Sort {
     }
 
     public void Merge(int[] array, int startIndex, int middle, int endIndex){
+        int leftArray[] = new int[middle - startIndex];    //these arrays are temporary so that we would not
+        int rightArray[] = new int[endIndex - middle + 1]; //have to replace the original positions
+
+        int i = 0;
+        while (i < middle - startIndex) {
+            leftArray[i] = array[i];
+            i++;
+        }
+        while (i < array.length) {
+            rightArray[i] = array[i];
+            i++;
+        }
+
+        i = 0;  //index of the original array as we will begin to rewrite it
+        int j = 0, k = 0; //indexes of the left and right arrays respectively
 
     }
 }
