ed4e81693d3f6b872a465956ba96269b47200c45
garen <garend95@gmail.com>
2017-09-06 18:21:40 +0400

    Declared the mergeSort function

--- homework/src/Merge_Sort.java
+++ homework/src/Merge_Sort.java
@@ -1,5 +1,21 @@
 public class Merge_Sort {
     public static void main(String[] Args) {
-        System.out.println("hello");
+
+        int array[] = {4,2,1,6,7,3,7,-4,0};
+
+
+    }
+
+    public void MergeSort(int[] array, int startIndex, int endIndex){
+        if (endIndex > startIndex){
+            int middle = startIndex + (endIndex - startIndex)/2 ;
+            MergeSort(array, startIndex, middle);
+            MergeSort(array, middle + 1, endIndex);
+
+        }
+    }
+
+    public void Merge(int[] array, int startIndex, int middle, int endIndex){
+
     }
 }
