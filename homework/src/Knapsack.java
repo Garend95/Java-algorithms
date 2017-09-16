9d7ac452207df581d5647feef332fc829b4a9f37
garen <garend95@gmail.com>
2017-09-16 17:21:31 +0400

    Initial code translate to java

---
+++ homework/src/Knapsack.java
@@ -0,0 +1,39 @@
+import java.util.Scanner;
+
+import static java.lang.System.out;
+
+public class Knapsack {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int a[] = new int[1000];
+        int D[][] = new int[1000][10000];
+        int n, M;
+
+        n = input.nextInt();
+        M = input.nextInt();
+
+        for (int i = 0; i < n; i++)
+            a[i] = input.nextInt();
+
+        for (int i = 1; i < n+1; i++)
+            for (int j = 1; j <= M; j++) {
+                if (j - a[i] >= 0)
+                    D[i][j] = Math.max(D[i-1][j], D[i-1][j-a[i]]);
+                else
+                    D[i][j] = D[i-1][j];
+            }
+        System.out.println(D[n][M]);
+    }
+
+    void print(int n, int M, int[] a, int[][] D) {
+        if (n==0)
+            return;
+        if (D[n][M] == D[n-1][M]) {
+            print(n-1, M, a, D);
+        } else
+        {
+            out.print(a[n] + ' ');
+            print(n-1,M - a[n], a, D);
+        }
+    }
+}
