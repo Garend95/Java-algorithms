385c40d387040f8f90ac265303d7b95998c8d3ef
garen <garend95@gmail.com>
2017-09-20 08:40:04 +0400

    fixed indexing and added ability to print array

--- homework/src/Knapsack.java
+++ homework/src/Knapsack.java
@@ -1,10 +1,9 @@
 import java.util.Scanner;
 
-import static java.lang.System.out;
-
 public class Knapsack {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
+        Knapsack k = new Knapsack();
         int a[] = new int[1000];
         int D[][] = new int[1000][10000];
         int n, M;
@@ -15,14 +14,23 @@ public class Knapsack {
         for (int i = 0; i < n; i++)
             a[i] = input.nextInt();
 
-        for (int i = 1; i < n+1; i++)
+        for (int i = 1; i < n; i++) {
             for (int j = 1; j <= M; j++) {
                 if (j - a[i] >= 0)
-                    D[i][j] = Math.max(D[i-1][j], D[i-1][j-a[i]]);
+                    D[i][j] = Math.max(D[i - 1][j], D[i - 1][j - a[i]] + a[i]);
                 else
-                    D[i][j] = D[i-1][j];
+                    D[i][j] = D[i - 1][j];
             }
-        System.out.println(D[n][M]);
+        }
+        //System.out.println(D[n][M]);
+
+        for (int i = 1; i < n; i++) {
+            for (int j = 1; j <= M; j++) {
+                System.out.print(D[i][j] + " ");
+            }
+            System.out.println();
+        }
+        k.print(n, M, a, D);
     }
 
     void print(int n, int M, int[] a, int[][] D) {
@@ -32,7 +40,7 @@ public class Knapsack {
             print(n-1, M, a, D);
         } else
         {
-            out.print(a[n] + ' ');
+            System.out.print(a[n] + ' ');
             print(n-1,M - a[n], a, D);
         }
     }
